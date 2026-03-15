92024a8c726069fb8ac96cf20da277d378142aa3
Pushpraj-Patidar <141001324+Pushpraj-Patidar@users.noreply.github.com>
2026-03-16 03:09:02 +0530

    Update SquareShell.js

--- SquareShell.js
+++ SquareShell.js
@@ -3,7 +3,7 @@ const BOUNDARY = 3; // Half the size of our pattern
 for(let i = -1*BOUNDARY; i <= BOUNDARY; i++){
     let currentRow = "";
     for(let j = -1*BOUNDARY; j <= BOUNDARY; j++){
-        currentRow += Math.max(Math.abs(i), Math.abs(j)) + 1) + " ";
+        currentRow += (Math.max(Math.abs(i), Math.abs(j)) + 1) + " ";
     }
     console.log(currentRow);
 }
